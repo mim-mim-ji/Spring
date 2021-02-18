0fa2aca4b73020730b590a0689da428190572660
minji <smj9030@gmail.com>
2021-02-18 23:44:46 +0900

    second commit

---
+++ SpringDBAccess/src/main/java/com/spring/db/jdbc/board/controller/BoardController.java
@@ -0,0 +1,95 @@
+package com.spring.db.jdbc.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.db.jdbc.board.model.BoardVO;
+import com.spring.db.jdbc.board.service.IBoardService;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@Autowired
+	private IBoardService service;
+	
+	//글 작성 화면 요청
+	@GetMapping("/write")
+	public void write() {
+		System.out.println("/board/write: GET");
+	}
+	
+	//글 작성 처리 요청
+	@PostMapping("/write")
+	public String write(BoardVO article) {
+		System.out.println("/board/write: POST");
+		service.insertArticle(article);
+		return "redirect:/board/list";
+		
+	}
+	
+	//글 목록 화면 요청
+	@GetMapping("/list")
+	public void list(Model model) {
+		System.out.println("/board/list: GET");
+		model.addAttribute("articles", service.getArticles());
+	}
+	
+	//글 내용보기 요청
+	@GetMapping("/content")
+	public void content(int boardNo, Model model) {
+		System.out.println("/board/content?boardNo=" + boardNo + ": GET");
+		model.addAttribute("article", service.getContent(boardNo));
+	}
+	
+	//글 수정하기 화면 요청
+	@GetMapping("/modify")
+	public void modify(int boardNo, Model model) {
+		System.out.println("/board/modify?boardNo=" + boardNo + ": GET");
+		model.addAttribute("article", service.getContent(boardNo));
+	}
+	
+	//글 수정 처리요청.
+	@PostMapping("/modify")
+	public String modify(BoardVO article) {
+		System.out.println("/board/modify?boardNo=" + article.getBoardNo() + ": POST");
+		service.modifyArticle(article);
+		return "redirect:/board/content?boardNo=" + article.getBoardNo();
+	}
+	
+	//글 삭제 요청
+	@GetMapping("/delete")
+	public String delete(int boardNo) {
+		System.out.println("/board/delete?boardNo=" + boardNo + ": GET");
+		service.deleteArticle(boardNo);
+		return "redirect:/board/list";
+	}
+	
+	//게시글 검색 처리 요청
+	@GetMapping("/searchList")
+	public String searchList(String keyword, Model model) {
+		model.addAttribute("articles", service.getSearchList(keyword));
+		return "board/list";
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SpringDBAccess/src/main/java/com/spring/db/jdbc/board/service/BoardService.java
@@ -0,0 +1,66 @@
+package com.spring.db.jdbc.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.db.jdbc.board.model.BoardVO;
+import com.spring.db.jdbc.board.repository.IBoardDAO;
+import com.spring.db.jdbc.board.repository.IBoardMapper;
+
+@Service
+public class BoardService implements IBoardService {
+	
+	//JDBC Template을 이용한 SQL 처리.
+//	@Autowired
+//	private IBoardDAO dao;
+
+	//MyBatis를 이용한 SQL 처리.
+	@Autowired
+	private IBoardMapper dao;
+		
+	@Override
+	public List<BoardVO> getArticles() {
+		return dao.getArticles();
+	}
+
+	@Override
+	public void insertArticle(BoardVO article) {
+		dao.insertArticle(article);
+	}
+
+	@Override
+	public void deleteArticle(int index) {
+		dao.deleteArticle(index);
+	}
+
+	@Override
+	public BoardVO getContent(int index) {
+		return dao.getContent(index);
+	}
+
+	@Override
+	public void modifyArticle(BoardVO article) {
+		dao.modifyArticle(article);
+	}
+	
+	@Override
+	public List<BoardVO> getSearchList(String keyword) {
+		keyword = "%" + keyword + "%";
+		return dao.getSearchList(keyword);
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
